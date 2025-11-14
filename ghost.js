7aabb592dab02ecb2aca048edc7d4679d3f2c0e6
Subeen-Kim-523 <sk13010@nyu.edu>
2025-11-14 00:39:08 -0500

    final version

--- ghost.js
+++ ghost.js
@@ -1,4 +1,4 @@
-let maxversion = 4; // Total number of ghost versions
+let maxversion = 7; // Total number of ghost versions
 
 class Ghost{
     constructor(x, y){
@@ -32,17 +32,17 @@ class Ghost{
             this.timer++;
             if (this.timer > 120) {
                 this.visible = true;
-                this.size += 0.15;
+                this.size = random(0.15, 2.5);
                 this.x = random(width * 0.3, width * 0.7);
                 this.y = random(height * 0.3, height * 0.6);
                 this.version = ((this.version + 1) % maxversion);
             }
         }
-        // Reset if the ghost becomes too large
-        if (this.size > 2.5) {
-            this.size = 0.5;
-            this.y = 300;
-        }
+        // // Reset if the ghost becomes too large
+        // if (this.size > 2.5) {
+        //     this.size = 0.5;
+        //     this.y = 300;
+        // }
     }
     
     ghostdraw(pg){
@@ -63,6 +63,15 @@ class Ghost{
                 break;
             case 4:
                 this.drawVersion4(pg);
+                break;
+            case 5:
+                this.drawVersion5(pg);
+                break;
+            case 6:
+                this.drawVersion6(pg);
+                break;
+            case 7:
+                this.drawVersion7(pg);
                 break;
         }
         pg.pop();
@@ -202,7 +211,7 @@ class Ghost{
 
         // Eyes
         pg.strokeWeight(1);
-        pg.fill(255);
+        pg.fill(255, 0, 0);
         pg.stroke(255, 0, 0);
         pg.bezier(-18, -10, -14, -17, -10, -17, -6, -10);
         pg.bezier(-18, -10, -14, -3, -10, -3, -6, -10);
@@ -210,10 +219,77 @@ class Ghost{
         pg.bezier(18, -10, 14, -17, 10, -17, 6, -10);
         pg.bezier(18, -10, 14, -3, 10, -3, 6, -10);
         
+        
+        // Nose
         pg.noStroke();
+        pg.ellipse(0, 3, 12, 12);
+        
+        // Mouth
+        pg.stroke(255, 0, 0);
+        pg.strokeWeight(4);
+        pg.fill(255);
+        pg.bezier(-20, 10, -10, 30, 10, 30, 20, 10);
+        
+        //Balloon
         pg.fill(255, 0, 0);
-        pg.ellipse(-12, -10, 4, 4);
-        pg.ellipse(12, -10, 4, 4);
+        pg.strokeWeight(1);
+        pg.stroke(255);
+        pg.rect(-55, -30, 1, 150);
+        
+        pg.noStroke();
+        pg.ellipse(-55, -60, 50, 60);
+        
+        //Arms
+        pg.noStroke();
+        pg.fill(255);
+        pg.quad(-15, 40, -40, 120, -50, 115, -25, 40);
+        pg.ellipse(-50, 120, 20, 20);
+        pg.quad(15, 40, 40, 120, 50, 115, 25, 40);
+        pg.ellipse(50, 120, 20, 20);
+
+        // Body + Clothes
+        pg.noStroke();
+        pg.fill("#7A0000");
+        pg.quad(-15, 35, -40, 155, 40, 155, 15, 35);
+        pg.fill("#E60026");
+        pg.ellipse(-15, 40, 20, 20);
+        pg.ellipse(0, 40, 20, 20);
+        pg.ellipse(15, 40, 20, 20);
+        
+        pg.ellipse(-40, 155, 30, 30);
+        pg.ellipse(-20, 155, 30, 30);
+        pg.ellipse(0, 155, 30, 30);
+        pg.ellipse(20, 155, 30, 30);
+        pg.ellipse(40, 155, 30, 30);
+        
+        // Legs
+        pg.ellipse(-15, 205, 30, 100);
+        pg.ellipse(15, 205, 30, 100);
+        pg.ellipse(-30, 250, 40, 20);
+        pg.ellipse(30, 250, 40, 20);
+    }
+
+    drawVersion5(pg){
+        // Head
+        pg.noStroke();
+        pg.fill(255);
+        pg.ellipse(0, 0, 50, 70);
+        
+        //Eyebrow
+        pg.stroke(0);
+        pg.strokeWeight(1);
+        pg.bezier(-18, -20, -14, -21, -10, -21, -6, -20);
+        pg.bezier(18, -20, 14, -21, 10, -21, 6, -20);
+
+        // Eyes
+        pg.strokeWeight(1);
+        pg.fill(255, 0, 0);
+        pg.stroke(255, 0, 0);
+        pg.bezier(-18, -10, -14, -17, -10, -17, -6, -10);
+        pg.bezier(-18, -10, -14, -3, -10, -3, -6, -10);
+        
+        pg.bezier(18, -10, 14, -17, 10, -17, 6, -10);
+        pg.bezier(18, -10, 14, -3, 10, -3, 6, -10);
         
         // Nose
         pg.noStroke();
@@ -229,6 +305,158 @@ class Ghost{
         pg.fill("#880808");
         pg.rect(-14, -3, 1, 1);
         pg.rect(13, -3, 1, 1);
+        
+        //Balloon
+        pg.fill(255, 0, 0);
+        pg.strokeWeight(1);
+        pg.stroke(255);
+        pg.rect(-55, -30, 1, 150);
+        
+        pg.noStroke();
+        pg.ellipse(-55, -60, 50, 60);
+        
+        //Arms
+        pg.noStroke();
+        pg.fill(255);
+        pg.quad(-15, 40, -40, 120, -50, 115, -25, 40);
+        pg.ellipse(-50, 120, 20, 20);
+        pg.quad(15, 40, 40, 120, 50, 115, 25, 40);
+        pg.ellipse(50, 120, 20, 20);
+
+        // Body + Clothes
+        pg.noStroke();
+        pg.fill("#7A0000");
+        pg.quad(-15, 35, -40, 155, 40, 155, 15, 35);
+        pg.fill("#E60026");
+        pg.ellipse(-15, 40, 20, 20);
+        pg.ellipse(0, 40, 20, 20);
+        pg.ellipse(15, 40, 20, 20);
+        
+        pg.ellipse(-40, 155, 30, 30);
+        pg.ellipse(-20, 155, 30, 30);
+        pg.ellipse(0, 155, 30, 30);
+        pg.ellipse(20, 155, 30, 30);
+        pg.ellipse(40, 155, 30, 30);
+        
+        // Legs
+        pg.ellipse(-15, 205, 30, 100);
+        pg.ellipse(15, 205, 30, 100);
+        pg.ellipse(-30, 250, 40, 20);
+        pg.ellipse(30, 250, 40, 20);
+    }
+
+    drawVersion6(pg){
+        // Head
+        pg.noStroke();
+        pg.fill(255);
+        pg.ellipse(0, 0, 50, 70);
+        
+        //Eyebrow
+        pg.stroke(0);
+        pg.strokeWeight(1);
+        pg.bezier(-18, -20, -14, -21, -10, -21, -6, -20);
+        pg.bezier(18, -20, 14, -21, 10, -21, 6, -20);
+
+        // Eyes
+        pg.strokeWeight(1);
+        pg.fill(255, 0, 0);
+        pg.stroke(255, 0, 0);
+        pg.bezier(-18, -10, -14, -17, -10, -17, -6, -10);
+        pg.bezier(-18, -10, -14, -3, -10, -3, -6, -10);
+        
+        pg.bezier(18, -10, 14, -17, 10, -17, 6, -10);
+        pg.bezier(18, -10, 14, -3, 10, -3, 6, -10);
+        
+        // Nose
+        pg.noStroke();
+        pg.ellipse(0, 3, 12, 12);
+        
+        // Mouth
+        pg.stroke(255, 0, 0);
+        pg.strokeWeight(4);
+        pg.fill(255);
+        pg.bezier(-20, 10, -10, 30, 10, 30, 20, 10);
+        
+        //Tear
+        pg.fill("#880808");
+        pg.rect(-14, -3, 1, 10);
+        pg.rect(13, -3, 1, 10);
+        
+        //Balloon
+        pg.fill(255, 0, 0);
+        pg.strokeWeight(1);
+        pg.stroke(255);
+        pg.rect(-55, -30, 1, 150);
+        
+        pg.noStroke();
+        pg.ellipse(-55, -60, 50, 60);
+        
+        //Arms
+        pg.noStroke();
+        pg.fill(255);
+        pg.quad(-15, 40, -40, 120, -50, 115, -25, 40);
+        pg.ellipse(-50, 120, 20, 20);
+        pg.quad(15, 40, 40, 120, 50, 115, 25, 40);
+        pg.ellipse(50, 120, 20, 20);
+
+        // Body + Clothes
+        pg.noStroke();
+        pg.fill("#7A0000");
+        pg.quad(-15, 35, -40, 155, 40, 155, 15, 35);
+        pg.fill("#E60026");
+        pg.ellipse(-15, 40, 20, 20);
+        pg.ellipse(0, 40, 20, 20);
+        pg.ellipse(15, 40, 20, 20);
+        
+        pg.ellipse(-40, 155, 30, 30);
+        pg.ellipse(-20, 155, 30, 30);
+        pg.ellipse(0, 155, 30, 30);
+        pg.ellipse(20, 155, 30, 30);
+        pg.ellipse(40, 155, 30, 30);
+        
+        // Legs
+        pg.ellipse(-15, 205, 30, 100);
+        pg.ellipse(15, 205, 30, 100);
+        pg.ellipse(-30, 250, 40, 20);
+        pg.ellipse(30, 250, 40, 20);
+    }
+
+    drawVersion7(pg){
+        // Head
+        pg.noStroke();
+        pg.fill(255);
+        pg.ellipse(0, 0, 50, 70);
+        
+        //Eyebrow
+        pg.stroke(0);
+        pg.strokeWeight(1);
+        pg.bezier(-18, -20, -14, -21, -10, -21, -6, -20);
+        pg.bezier(18, -20, 14, -21, 10, -21, 6, -20);
+
+        // Eyes
+        pg.strokeWeight(1);
+        pg.fill(255, 0, 0);
+        pg.stroke(255, 0, 0);
+        pg.bezier(-18, -10, -14, -17, -10, -17, -6, -10);
+        pg.bezier(-18, -10, -14, -3, -10, -3, -6, -10);
+        
+        pg.bezier(18, -10, 14, -17, 10, -17, 6, -10);
+        pg.bezier(18, -10, 14, -3, 10, -3, 6, -10);
+        
+        // Nose
+        pg.noStroke();
+        pg.ellipse(0, 3, 12, 12);
+        
+        // Mouth
+        pg.stroke(255, 0, 0);
+        pg.strokeWeight(4);
+        pg.fill(255);
+        pg.bezier(-20, 10, -10, 30, 10, 30, 20, 10);
+        
+        //Tear
+        pg.fill("#880808");
+        pg.rect(-14, -3, 1, 15);
+        pg.rect(13, -3, 1, 15);
         
         //Balloon
         pg.fill(255, 0, 0);
